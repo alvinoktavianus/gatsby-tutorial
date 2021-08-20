713f403ff1e8eb174ab318f2f05f58e20ea9df50
Alvin Oktavianus <agentsam1995@gmail.com>
2021-08-20 10:47:56 +0700

    add babel plugin macro, stylelint, prettier, eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-tutorial",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'gatsby-tutorial',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
   ],
-};
+}
